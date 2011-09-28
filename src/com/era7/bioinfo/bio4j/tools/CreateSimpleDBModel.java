5372092ee27f59386da70a10adf6d3ada3141aa1
Pablo Pareja Tobes <ppareja@era7.com>
2011-09-28 14:23:33 +0200

    first commit in develop branch

---
+++ src/com/era7/bioinfo/bio4j/tools/CreateSimpleDBModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.era7.bioinfo.bio4j.tools;
+
+
+/**
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public class CreateSimpleDBModel{
+   
+    
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }
+    
+}
