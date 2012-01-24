41cad19b51fe9296851990f35dbab8a864b52420
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2012-01-24 13:22:56 +0100

    adding program for removing repetitions from PPI circuits of length 3

---
+++ src/com/era7/bioinfo/bio4j/tools/RemoveRepetitionsFromPPICircuits.java
@@ -0,0 +1,118 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.era7.bioinfo.bio4j.tools;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public class RemoveRepetitionsFromPPICircuits {
+    
+    public static void main(String[] args){
+        
+        if(args.length != 3){
+            System.out.println("This program expects the following parameters:\n" +
+                    "1. Input file name including the PPI circuits\n" +
+                    "2. Output results file name\n" +
+                    "3. Output proteins frequency file name\n"); 
+        }else{
+            try {
+                
+                BufferedReader buff = new BufferedReader(new FileReader(new File(args[0])));
+                String line = null;
+                
+                BufferedWriter outBuff = new BufferedWriter(new FileWriter(new File(args[1])));
+                BufferedWriter protBuff = new BufferedWriter(new FileWriter(new File(args[2])));
+                
+                LinkedList<LinkedList<String>> list = new LinkedList<LinkedList<String>>();
+                HashMap<String, Integer> proteinCounter = new HashMap<String, Integer>();
+                
+                System.out.println("Reading file...");
+                int repetitionsCounter = 0;
+                
+                while((line = buff.readLine()) != null){
+                    String[] columns = line.trim().split(" ");
+                    boolean repeated = false;
+                    for (LinkedList<String> linkedList : list) {
+                        if( (linkedList.get(0).equals(columns[0]) && linkedList.get(1).equals(columns[1]) && linkedList.get(2).equals(columns[2]) ) ||
+                            (linkedList.get(0).equals(columns[1]) && linkedList.get(1).equals(columns[2]) && linkedList.get(2).equals(columns[0]) ) ||   
+                            (linkedList.get(0).equals(columns[2]) && linkedList.get(1).equals(columns[0]) && linkedList.get(2).equals(columns[1]) ) ){
+                            
+                            repeated = true;
+                            repetitionsCounter++;
+                            break;
+                            
+                        }
+                    }
+                    if(!repeated){
+                        LinkedList<String> lList = new LinkedList<String>();
+                        lList.add(columns[0]);
+                        lList.add(columns[1]);
+                        lList.add(columns[2]);
+                        list.add(lList);
+                    }
+                }               
+                
+                buff.close();
+                
+                System.out.println("Done!");
+                
+                System.out.println(repetitionsCounter + " repetitions were found...");
+                
+                System.out.println("Writing results!");
+                
+                for (LinkedList<String> linkedList : list) {
+                    Iterator<String> it = linkedList.iterator();  
+                    String tempSt = "";                    
+                    while(it.hasNext()){
+                        String protSt = it.next();
+                        tempSt = tempSt + protSt + "\t";
+                        
+                        //updating prot counters
+                        Integer counter = proteinCounter.get(protSt);
+                        if(counter == null){
+                            proteinCounter.put(protSt, 1);
+                        }else{
+                            proteinCounter.put(protSt, (counter + 1));
+                        }                        
+                    }
+                    tempSt = tempSt.substring(0,tempSt.length() - 1) + "\n";
+                    outBuff.write(tempSt);
+                }
+                
+                outBuff.close();
+                
+                Set<String> proteinSet = proteinCounter.keySet();
+                for (String protSt : proteinSet) {
+                    protBuff.write(protSt + "\t" + proteinCounter.get(protSt) + "\n");
+                }                
+                
+                protBuff.close();
+                
+                System.out.println("done! ;)");
+                
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }       
+            
+            
+            
+        }
+        
+    }
+    
+}
