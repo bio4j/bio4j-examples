5de4afcc86ab8fc292aefd02423befca38708751
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2012-01-26 15:37:49 +0100

    some updates

--- src/com/era7/bioinfo/bio4j/tools/uniref/FindBiggestUniref100Clusters.java
+++ src/com/era7/bioinfo/bio4j/tools/uniref/FindBiggestUniref100Clusters.java
@@ -7,9 +7,15 @@ package com.era7.bioinfo.bio4j.tools.uniref;
 import com.era7.bioinfo.bio4jmodel.nodes.ProteinNode;
 import com.era7.bioinfo.bio4jmodel.relationships.uniref.UniRef100MemberRel;
 import com.era7.bioinfo.bio4jmodel.util.Bio4jManager;
-import com.era7.bioinfo.bio4jmodel.util.NodeRetriever;
+import com.google.common.collect.Collections2;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.TreeMap;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
@@ -21,7 +27,8 @@ import org.neo4j.graphdb.Relationship;
  */
 public class FindBiggestUniref100Clusters {
 
-    public static void main(String[] args) {
+   
+    public static void main(String[] args) throws IOException {
 
         if (args.length != 3) {
             System.out.println("This program expects the following parameters:\n"
@@ -30,12 +37,13 @@ public class FindBiggestUniref100Clusters {
                     + "3. Output results file name (txt)\n");
         } else {
 
-            File outFile = new File(args[1]);
+            File outFile = new File(args[2]);
+
+            BufferedWriter outBuff = new BufferedWriter(new FileWriter(outFile));
 
             Bio4jManager manager = new Bio4jManager(args[0], args[1], true);
-            NodeRetriever nodeRetriever = new NodeRetriever(manager);
 
-            TreeMap<String, Integer> clustersCountersMap = new TreeMap<String, Integer>();
+            List<ProteinCounter> list = new LinkedList<ProteinCounter>();
 
             int protCounter = 0;
 
@@ -43,19 +51,19 @@ public class FindBiggestUniref100Clusters {
             Iterator<Node> iterator = manager.getNodeTypeIndex().get(Bio4jManager.NODE_TYPE_INDEX_NAME, ProteinNode.NODE_TYPE).iterator();
 
             UniRef100MemberRel uniRef100MemberRel = new UniRef100MemberRel(null);
-            
+
             while (iterator.hasNext()) {
                 ProteinNode protein = new ProteinNode(iterator.next());
                 if (protein.isUniref100Representant()) {
                     int clusterLength = 1;
 
                     Iterator<Relationship> relIterator = protein.getNode().getRelationships(Direction.OUTGOING, uniRef100MemberRel).iterator();
-                    while(relIterator.hasNext()){
+                    while (relIterator.hasNext()) {
                         relIterator.next();
                         clusterLength++;
                     }
-                    
-                    clustersCountersMap.put(protein.getAccession(), clusterLength);
+
+                    list.add(new ProteinCounter(protein.getAccession(), clusterLength));
                 }
 
                 protCounter++;
@@ -66,8 +74,46 @@ public class FindBiggestUniref100Clusters {
             }
 
             manager.shutDown();
+
+            System.out.println("Done!");
+            
+            System.out.println("Sorting values...");
+            
+            Collections.sort(list);
+            
+            System.out.println("Writing now the size of all clusters!");
+
+            for (int i = 0; i < 10; i++) {
+                ProteinCounter proteinCounter = list.get(i);
+                outBuff.write(proteinCounter.protein + "\t" + proteinCounter.counter + "\n");
+            }
+            
+            
+            outBuff.close();
+            
+            System.out.println("Done ;)");
         }
 
-    }   
+    }
+}
+
+class ProteinCounter implements Comparable<ProteinCounter> {
     
+    public String protein;
+    public int counter;
+    
+    public ProteinCounter(String prot, int c){
+        protein = prot;
+        counter = c;
+    }
+
+    public int compareTo(ProteinCounter o) {
+        if(o.counter > this.counter){
+            return -1;
+        }else if(o.counter < this.counter){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 }
