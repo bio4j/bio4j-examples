20aa13b0545b4ebcf76ac2e9d11af6416c8fb9cc
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2013-02-15 11:19:07 +0100

    updating stuff after refactorings carried out in Bio4j project

--- src/com/era7/bioinfo/bio4j/tools/gephi/GetGexfForGenomeElementUnirefClusters.java
+++ src/com/era7/bioinfo/bio4j/tools/gephi/GetGexfForGenomeElementUnirefClusters.java
@@ -16,12 +16,12 @@
  */
 package com.era7.bioinfo.bio4j.tools.gephi;
 
-import com.era7.bioinfo.bio4j.model.nodes.OrganismNode;
-import com.era7.bioinfo.bio4j.model.nodes.ProteinNode;
-import com.era7.bioinfo.bio4j.model.nodes.refseq.GenomeElementNode;
-import com.era7.bioinfo.bio4j.model.relationships.protein.ProteinGenomeElementRel;
-import com.era7.bioinfo.bio4j.model.util.Bio4jManager;
-import com.era7.bioinfo.bio4j.model.util.NodeRetriever;
+import com.era7.bioinfo.bio4j.neo4j.model.nodes.OrganismNode;
+import com.era7.bioinfo.bio4j.neo4j.model.nodes.ProteinNode;
+import com.era7.bioinfo.bio4j.neo4j.model.nodes.refseq.GenomeElementNode;
+import com.era7.bioinfo.bio4j.neo4j.model.relationships.protein.ProteinGenomeElementRel;
+import com.era7.bioinfo.bio4j.neo4j.model.util.Bio4jManager;
+import com.era7.bioinfo.bio4j.neo4j.model.util.NodeRetriever;
 import com.era7.lib.bioinfo.bioinfoutil.Executable;
 import com.era7.lib.bioinfoxml.gexf.*;
 import com.era7.lib.bioinfoxml.gexf.viz.VizColorXML;
