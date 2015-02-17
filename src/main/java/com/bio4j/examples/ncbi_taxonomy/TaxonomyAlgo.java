a17a7cfd5343f8094edc4b2cba56e1f67cd9e383
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2015-02-17 18:22:01 +0100

    some progress on the update of TaxonomyAlgo

---
+++ src/main/java/com/bio4j/examples/ncbi_taxonomy/TaxonomyAlgo.java
@@ -0,0 +1,92 @@
+
+package com.bio4j.examples.ncbi_taxonomy;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import com.bio4j.model.ncbiTaxonomy.vertices.NCBITaxon;
+import com.bio4j.titan.util.DefaultTitanGraph;
+import com.thinkaurelius.titan.core.TitanEdge;
+import com.thinkaurelius.titan.core.TitanVertex;
+import com.thinkaurelius.titan.core.schema.EdgeLabelMaker;
+import com.thinkaurelius.titan.core.schema.VertexLabelMaker;
+
+
+public class TaxonomyAlgo {
+    
+    
+    public static NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker> lowestCommonAncestor(List<NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker>> nodes){
+     
+        
+        //NCBITaxonNode lowerCommonAncestor = null;
+        
+        if(nodes.size() > 0){
+
+	        NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker> firstNode = nodes.get(0);
+            
+            LinkedList<NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker>> firstAncestors = getAncestorsPlusSelf(firstNode);
+            
+            for (int i = 1; i < nodes.size() && !firstAncestors.isEmpty(); i++) {
+	            NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker> currentNode = nodes.get(i);
+                lookForCommonAncestor(firstAncestors, currentNode);                
+            }
+            
+            
+            if(!firstAncestors.isEmpty()){
+                return firstAncestors.get(0);
+            }else{
+	            return null;
+            }
+            
+            //------freeing space---------
+            firstAncestors.clear();
+
+
+        }else{
+	        return null;
+        }
+
+    }
+    
+    
+    
+    private static LinkedList<NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker>> getAncestorsPlusSelf(NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker> taxon){
+        
+        LinkedList<NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker>> ancestors = new LinkedList<>();
+        
+        ancestors.add(taxon);
+
+	    Optional<NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker>> taxonParentOptional = taxon.ncbiTaxonParent_inV();
+                
+        while(taxonParentOptional.isPresent()){
+	        NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker> taxonParent = taxonParentOptional.get();
+            ancestors.add(taxonParent);
+	        taxonParentOptional = taxonParent.ncbiTaxonParent_inV();
+        }
+        
+        return ancestors;
+        
+    }
+    
+
+    private static void lookForCommonAncestor(LinkedList<NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker>> commonAncestors, NCBITaxon<DefaultTitanGraph, TitanVertex, VertexLabelMaker, TitanEdge, EdgeLabelMaker> currentNode){
+        
+        while(currentNode != null){
+            
+            for (int i = 0; i < commonAncestors.size(); i++) {
+                NCBITaxonNode nCBITaxonNode = commonAncestors.get(i);
+                if(nCBITaxonNode.getTaxId().equals(currentNode.getTaxId())){
+                    for(int j=0; j<i; j++){
+                        commonAncestors.pollFirst();
+                    }
+                    return;
+                }
+            }
+            
+            currentNode = currentNode.getParent();
+        }
+        
+    }
+
+}
